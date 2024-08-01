e4e8e1311db9dd199f0d2bc621ae11dde54c9c52
Perseverance Munga <percymunga02@gmail.com>
2024-08-01 14:24:38 +0100

    Fix lint for sign in user

--- src/features/authentication/useLogin.ts
+++ src/features/authentication/useLogin.ts
@@ -2,14 +2,14 @@ import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useNavigate } from "react-router-dom"
 import { toast } from "react-toastify"
 import { login as loginApi } from "../../services/apiAuth"
-import { SignupProps } from "../../types"
+import { SignInProps } from "../../types"
 
 export function useLogin() {
   const queryClient = useQueryClient()
   const navigate = useNavigate()
 
   const { mutate: login, isPending } = useMutation({
-    mutationFn: ({ email, password }: SignupProps) =>
+    mutationFn: ({ email, password }: SignInProps) =>
       loginApi({ email, password }),
     onSuccess: (user) => {
       queryClient.setQueryData(["user"], user.user)
