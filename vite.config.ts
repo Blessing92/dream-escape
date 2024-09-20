96f748a48127776cca37b8d26d262566eecd31e9
Perseverance Munga <percymunga02@gmail.com>
2024-09-20 19:10:51 +0100

    Update config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,38 @@
-import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
+import { visualizer } from "rollup-plugin-visualizer"
+import { defineConfig, loadEnv } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  // expose .env as process.env instead of process.env
+  // Reference: https://github.com/vitejs/vite/issues/1149#issuecomment-857686209
   const env = loadEnv(mode, process.cwd(), "VITE_APP")
 
+  // Optional: Populate NODE_ENV with the current mode (development/production)
   env.NODE_ENV = mode
+
+  const envWithProcessPrefix = {
+    "process.env": `${JSON.stringify(env)}`,
+  }
+
+  const sentryActive =
+    !!env.VITE_APP_SENTRY_DSN && !!process.env.SENTRY_AUTH_TOKEN
 
   return {
     build: {
+      // Relative to the root
       outDir: "./build",
-      sourcemap: true,
+      sourcemap: true, // Source map generation must be turned on
       target: "es2022",
       rollupOptions: {
         output: {
           manualChunks(id) {
             if (id.includes("node_modules")) {
-              return id
-                .toString()
-                .split("node_modules/")[1]
-                .split("/")[0]
-                .toString()
+              if (id.includes("@supabase") || id.includes("@chakra-ui")) {
+                const libraryPath = id.split("node_modules/")[1]
+                return `${libraryPath.split("/")[0]}/${libraryPath.split("/")[1]}`
+              }
+              return id.split("node_modules/")[1].split("/")[0]
             }
             return null
           },
@@ -44,11 +56,22 @@ export default defineConfig(({ mode }) => {
       setupFiles: ["./src/setupTests.ts"],
       testMatch: ["./tests/*.test.ts?(x)"],
       globals: true,
+      deps: {
+        optimizer: {
+          ssr: {
+            include: ["@epicbrief/shared"],
+          },
+        },
+      },
     },
-    plugins: [react({ include: "**/*.{js,jsx,tsx}" })],
-    define: {
-      "process.env": `${JSON.stringify(env)}`,
-      __APP_ENV__: env.APP_ENV,
-    },
+    plugins: [
+      visualizer(),
+      // …
+      react({
+        // Use React plugin in all *.jsx and *.tsx files
+        include: "**/*.{js,jsx,tsx}",
+      }),
+    ],
+    define: envWithProcessPrefix,
   }
 })
